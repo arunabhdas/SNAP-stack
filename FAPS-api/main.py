bb16a6d829ed0195571adc9f7bde3c1a39fc5b38
Arunabh Das <arunabhdas@gmail.com>
2023-03-23 07:58:01 -0400

    Added admin auth

--- FAPS-api/main.py
+++ FAPS-api/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI, Depends, Response, status, HTTPException
+from typing import Optional
 from sqladmin import Admin, ModelView
+from sqladmin.authentication import AuthenticationBackend
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
 
 from schemas import Post
 from router import post
@@ -14,7 +18,37 @@ from hashing import Hash
 ###############################################################################
 app = FastAPI()
 
-admin = Admin(app, engine)
+
+###############################################################################
+class AdminAuth(AuthenticationBackend):
+    async def login(self, request: Request) -> bool:
+        form = await request.form()
+        username, password = form["username"], form["password"]
+
+        # Validate username/password credentials
+        # And update session
+        request.session.update({"token": "..."})
+
+        return True
+
+    async def logout(self, request: Request) -> bool:
+        # Usually you'd want to just clear the session
+        request.session.clear()
+        return True
+
+    async def authenticate(self, request: Request) -> Optional[RedirectResponse]:
+        token = request.session.get("token")
+
+        if not token:
+            return RedirectResponse(request.url_for("admin:login"), status_code=302)
+
+        # Check the token in depth
+
+
+authentication_backend = AdminAuth(secret_key="12345")
+admin = Admin(app=app, engine=engine, authentication_backend=authentication_backend)
+
+# admin = Admin(app, engine)
 
 class UserAdmin(ModelView, model=models.User):
     can_create = True
