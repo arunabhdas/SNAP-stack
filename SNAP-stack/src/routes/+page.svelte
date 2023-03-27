99975a7e84500a329fbefca968b7c3a0ae7b0c53
Arunabh Das <arunabhdas@gmail.com>
2023-03-27 03:15:38 -0400

    Improved roundedness of card and input

--- SNAP-stack/src/routes/+page.svelte
+++ SNAP-stack/src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <div class="flex justify-center">
     <div class="grid grid-cols-1 gap-4 min-w-full md:min-w-[750px]">
         <h3 class="text-center py-6">Create an AI prompt</h3>
+        <div class="card p-4 w-full text-token space-y-4 rounded-lg">
+            <label class="label">
+                <span>Prompt Title</span>
+                <input class="input rounded-lg" type="text" placeholder="Enter prompt"/>
+            </label>
+        </div>
     </div>
 </div>
